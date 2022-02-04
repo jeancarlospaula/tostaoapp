06f1d6f74385a0229fc36abd8b3a2f06e4e4ec75
LAPTOP-46EMOVH7\Pessoal <jeandepaula294@gmail.com>
2022-02-04 20:01:34 -0300

    - Adicao de arquivo de contexto e conexao ao data base

---
+++ TostaoApp/Data/TostaoDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TostaoApp.Models;
+
+namespace TostaoApp.Data
+{
+    public class TostaoDataContext : DbContext
+    {
+        public DbSet<TransacaoModel> Transacaos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=;Database=;User ID=;Password=");
+    }
+}
