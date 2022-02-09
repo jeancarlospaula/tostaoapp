b1afffc43455dccbc53baac9c3677583fa3c6ce4
LAPTOP-46EMOVH7\Pessoal <jeandepaula294@gmail.com>
2022-02-09 07:39:25 -0300

    - Modificacao DbContext e adicao de novos models

--- TostaoApp/_App/App_Data/TostaoDataContext.cs
+++ TostaoApp/_App/App_Data/TostaoDataContext.cs
@@ -11,8 +11,10 @@ namespace TostaoApp.Data
     public class TostaoDataContext : DbContext
     {
         public DbSet<TransacaoModel> Transacaos { get; set; }
+        public DbSet<CategoriaModel> Categorias { get; set; }
+        public DbSet<TipoModel> Tipos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlServer("Server=;Database=;User ID=;Password=");
+            => optionsBuilder.UseSqlServer("Server=;Database=;User ID=;Password=;MultipleActiveResultSets=true;");
     }
 }
